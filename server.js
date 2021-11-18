6038aa730e7afe079805656d46bfe1e8e14048fa
Garazi <ggartzia@gmail.com>
2021-11-18 12:38:23 +0100

    Update server.js

--- server.js
+++ server.js
@@ -11,12 +11,17 @@ app.use(favicon(__dirname + '/build/favicon.ico'));
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/ping', function (req, res) {
- return res.send('pong');
+app.get('/', function (req, res) {
+  res.render("index", { titulo: "inicio EJS" });
 });
 
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.get("/nosotros", (req, res) => {
+  // res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.render("nosotros", { titulo: "Nosotros EJS" });
+});
+
+app.use(function (req, res, next) {
+  res.status(404).render("404", { titulo: "Página 404" });
 });
 
 app.listen(port);
