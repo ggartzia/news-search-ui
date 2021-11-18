1c1c6fd6115e9b3d034e5b98865bda1a351389ed
Garazi <ggartzia@gmail.com>
2021-11-18 13:01:35 +0100

    Update server.js

--- server.js
+++ server.js
@@ -9,24 +9,18 @@ app.use(favicon(__dirname + '/build/favicon.ico'));
 
 // motor de plantillas
 app.set('view engine', 'ejs');
-app.set('views', __dirname + '/views');
-
 
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'build')));
 app.use(express.static(path.join(__dirname, 'views')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function (req, res) {
   res.render(path.join(__dirname, 'views', 'index'), { titulo: "inicio EJS" });
 });
 
-app.get("/nosotros", (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'index'), { titulo: "inicio EJS" });
-});
-
-app.use(function (req, res, next) {
-  res.status(404).render("404", { titulo: "Página 404" });
+app.get("/nosotros", function (req, res) {
+  res.render(path.join(__dirname, 'views', 'index'), { titulo: "Nosotros" });
 });
 
 app.listen(port);
