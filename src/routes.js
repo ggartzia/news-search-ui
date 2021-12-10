414d4db2d1b27d4b60ea2e81fd752d598762f068
Garazi <ggartzia@gmail.com>
2021-12-10 09:42:47 +0100

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,7 @@
 import UltimasNoticias from "./layout/ultimas-noticias";
+import Trending from "./layout/trending";
 import Medios from "./layout/medios";
+import Temas from "./layout/temas";
 import Cuentas from "./layout/cuentas";
 
 
@@ -7,27 +9,38 @@ const routes = [
   { type: "title", title: "Noticias", key: "noticias" },
   {
     type: "link",
-    name: "Últimas noticias",
+    title: "Últimas noticias",
     key: "ultimas-noticias",
     route: "/",
-    icon: "",
     component: UltimasNoticias
+  },
+    {
+    type: "link",
+    title: "Noticias más comentadas",
+    key: "trending",
+    route: "/trending",
+    component: Trending
   },
   {
     type: "link",
-    name: "Medios de comunicación",
+    title: "Medios de comunicación",
     key: "medios",
     route: "/medios",
-    icon: "",
     component: Medios
+  },
+  {
+    type: "link",
+    title: "Temas más comentados",
+    key: "temas",
+    route: "/temas",
+    component: Temas
   },
   { type: "title", title: "Cuentas Twitter", key: "cuentas-twitter" },
   {
     type: "link",
-    name: "Cuentas más activas",
+    title: "Cuentas más activas",
     key: "cuentas",
     route: "/cuentas",
-    icon: "",
     component: Cuentas
   }
 ];
