27768236737bdf738f57d3bef7dcde497b38b163
Garazi <ggartzia@gmail.com>
2021-12-27 09:47:36 +0100

    Call API to load media

--- src/layout/medios/index.js
+++ src/layout/medios/index.js
@@ -1,3 +1,5 @@
+import React from "react";
+
 import Layout from "../../component/Layout";
 import Header from "../../component/Header";
 import Lista from "../../component/Lista";
@@ -5,74 +7,120 @@ import Box from "../../component/Box";
 import Typography from "../../component/Typography";
 import Avatar from "../../component/Avatar";
 
-import data from "./medios.json";
+class Medios extends React.Component {
 
-function obtenerDatos() {
-  return data.map( (data) => {
-    return {
-      nombre: (
-        <Box display="flex" alignItems="center" px={1} py={0.5}>
-          <Box mr={2}>
-            <Avatar src={data.logo} alt={data.paper} size="sm" variant="rounded" />
+  constructor(props) {
+      super(props);
+
+      this.state = {
+          mediosNoticias: [],
+          mediosDeportes: [],
+          mediosCorazon: []
+      };
+  }
+  
+  
+  componentDidMount() {
+    fetch("https://news-puller.herokuapp.com/get/media/noticias")
+        .then((res) => res.json())
+        .then((json) => {
+            currentState = this.state;
+            currentState.mediosNoticias = json
+            this.setState(currentState);
+        });
+    
+    fetch("https://news-puller.herokuapp.com/get/media/deportes")
+        .then((res) => res.json())
+        .then((json) => {
+            currentState = this.state;
+            currentState.mediosDeportes = json
+            this.setState(currentState);
+        });
+    
+    fetch("https://news-puller.herokuapp.com/get/media/corazon")
+        .then((res) => res.json())
+        .then((json) => {
+            currentState = this.state;
+            currentState.mediosCorazon = json
+            this.setState(currentState);
+        });
+  }
+    
+  
+  obtenerDatos(items) {
+    return items.map( (data) => {
+      return {
+        nombre: (
+          <Box display="flex" alignItems="center" px={1} py={0.5}>
+            <Box mr={2}>
+              <Avatar src={data.logo} alt={data.paper} size="sm" variant="rounded" />
+            </Box>
+            <Box display="flex" flexDirection="column">
+              <Typography variant="button" fontWeight="medium">
+                {data.paper}
+              </Typography>
+              <Typography variant="caption" color="secondary">
+                {data.feed}
+              </Typography>
+            </Box>
           </Box>
-          <Box display="flex" flexDirection="column">
-            <Typography variant="button" fontWeight="medium">
-              {data.paper}
-            </Typography>
-            <Typography variant="caption" color="secondary">
-              {data.feed}
-            </Typography>
+        ),
+        tema: (
+          <Typography variant="caption" color="secondary" fontWeight="medium">
+            {data.topic}
+          </Typography>
+        ),
+        noticias: (
+          <Typography variant="caption" color="secondary" fontWeight="medium">
+            {data.numeroNoticias}
+          </Typography>
+        ),
+        actualizacion: (
+          <Typography variant="caption" color="secondary" fontWeight="medium">
+            {data.actualizacion}
+          </Typography>
+        ),
+        accion: (
+          <Typography
+            component="a"
+            href="#"
+            variant="caption"
+            color="secondary"
+            fontWeight="medium" >
+            Ejecutar
+          </Typography>
+        )
+      };
+    });
+  }
+  
+  render() {
+    const columns = [
+      { name: "nombre", align: "left" },
+      { name: "tema", align: "left" },
+      { name: "noticias", align: "center" },
+      { name: "actualizacion", align: "center" },
+      { name: "accion", align: "center" },
+    ];
+    
+    const { mediosNoticias, mediosDeportes, mediosCorazon } = this.state;
+    
+    return (
+        <Layout>
+          <Header title="Medios utilizados" />
+          <Box sx={{ mx: 6, py: 6, px: 2 }}>
+            <Lista columns={columns} rows={obtenerDatos(mediosNoticias)} />
           </Box>
-        </Box>
-      ),
-      tema: (
-        <Typography variant="caption" color="secondary" fontWeight="medium">
-          {data.topic}
-        </Typography>
-      ),
-      noticias: (
-        <Typography variant="caption" color="secondary" fontWeight="medium">
-          {data.numeroNoticias}
-        </Typography>
-      ),
-      actualizacion: (
-        <Typography variant="caption" color="secondary" fontWeight="medium">
-          {data.actualizacion}
-        </Typography>
-      ),
-      accion: (
-        <Typography
-          component="a"
-          href="#"
-          variant="caption"
-          color="secondary"
-          fontWeight="medium" >
-          Ejecutar
-        </Typography>
-      )
-    };
-  });
+          <Box sx={{ mx: 6, py: 6, px: 2 }}>
+            <Lista columns={columns} rows={obtenerDatos(mediosDeportes)} />
+          </Box>
+           <Box sx={{ mx: 6, py: 6, px: 2 }}>
+            <Lista columns={columns} rows={obtenerDatos(mediosCorazon)} />
+          </Box>
+        </Layout>
+      );
+  }
 }
 
-function Medios() {
-
-  const columns = [
-    { name: "nombre", align: "left" },
-    { name: "tema", align: "left" },
-    { name: "noticias", align: "center" },
-    { name: "actualizacion", align: "center" },
-    { name: "accion", align: "center" },
-  ];
-  const rows = obtenerDatos();
-
-  return (
-    <Layout>
-      <Header title="Medios utilizados" />
-      <Box sx={{ mx: 6, py: 6, px: 2 }}>
-        <Lista columns={columns} rows={rows} />
-      </Box>
-    </Layout>
-  );
-}
 
 export default Medios;
