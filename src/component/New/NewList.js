91e103452395a31d58e0eb466b28225c27d680fc
Gartzia Gartzia <g-gartzia@lanbide.eus>
2022-06-20 14:07:26 +0200

    Replace data with new on related

--- src/component/New/NewList.js
+++ src/component/New/NewList.js
@@ -10,56 +10,55 @@ import Box from "../Box";
 import Typography from "../Typography";
 
 function NewList({ data }) {
-  article = data.new ? data.new : data;
-  const mediaLogo = `/medio/${article.paper}.jpg`;
+  const mediaLogo = `/medio/${data.paper}.jpg`;
 
   return (
-      <Grid item ml={3} mt={2} mb={2} style={{width: "30%"}} key={article._id}>
+      <Grid item ml={3} mt={2} mb={2} style={{width: "30%"}} key={data._id}>
         <Card>
           <Box component="a"
-               href={article.fullUrl}
+               href={data.fullUrl}
                display="flex"
                alignItems="center"
                px={2}
                py={2}>
             <Box mr={2}>
-              <Avatar src={article.image}
-                      alt={article.title}
+              <Avatar src={data.image}
+                      alt={data.title}
                       variant="square"
                       sx={{ width: 100, height: 100 }} />
             </Box>
             <Box display="flex" flexDirection="column">
               <Typography variant="button" fontWeight="medium">
-                {article.title}
+                {data.title}
               </Typography>
             </Box>
           </Box>
 
           <Box display="flex" alignItems="center" px={1} mb={1}>
             <Box mr={2}>
-              <Avatar src={mediaLogo} alt={article.paper} size="s" />
+              <Avatar src={mediaLogo} alt={data.paper} size="s" />
             </Box>
             <Box display="flex" flexDirection="column">
               <Typography variant="button" fontWeight="medium">
-                {article.paper}
+                {data.paper}
               </Typography>
               <Typography variant="caption" color="secondary">
-                Publicado hace {findDayDifference(article.published)}
+                Publicado hace {findDayDifference(data.published)}
               </Typography>
             </Box>
           </Box>
           <Grid container 
                 px={1} 
                 sx={{ justifyContent: 'space-between' }}>
-            {article.topics.slice(0, 3).map((topic) => renderTopic(topic))}
+            {data.topics.slice(0, 3).map((topic) => renderTopic(topic))}
           </Grid>
           <Grid container
                 px={3}
                 pt={1} 
                 pb={0.5} 
                 >
-            {noticiasRelacionadas(article._id)}
-            {twitterShare(article)}
+            {noticiasRelacionadas(data._id)}
+            {twitterShare(data)}
           </Grid>
         </Card>
       </Grid>
